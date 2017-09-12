ed79929fa00c0b3dc414f5fed395a61625fba650
CS70 Mink <lights925@gmail.com>
2017-09-11 20:33:54 -0700

    Added some basic Resources.

---
+++ Assets/Resources/HullIntegrity.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+// Hull Integrity represents the health of the ship. When hull integrity reaches zero, the ship has been destroyed.
+
+namespace AssemblyCSharp
+{
+	public class HullIntegrity
+	{
+		int maximumHullIntegrity;
+		int currentHullIntegrity;
+
+		// When we use the default constructor, the hull integrity starts at its maximum potential hull integrity.
+		public HullIntegrity (int maxHullIntegrity)
+		{
+			maximumHullIntegrity = maxHullIntegrity;
+			currentHullIntegrity = maxHullIntegrity;
+		}
+
+		// This constructor is for when we want the hull integrity to not start at its potential maximum. 
+		public HullIntegrity (int maxHullIntegrity, int startingHullIntegrity)
+		{
+			maximumHullIntegrity = maxHullIntegrity;
+			currentHullIntegrity = startingHullIntegrity;
+		}
+
+		// This method is called when the hull loses integrity.
+		void loseHullIntegrity (int hullIntegrityLost) 
+		{
+			if ((currentHullIntegrity -= hullIntegrityLost) < 0) {
+				currentHullIntegrity = 0;
+			} 
+
+			else {
+				currentHullIntegrity -= hullIntegrityLost;
+			}
+		}
+
+		// This method is called when the hull gains integrity.
+		void addHullIntegrity (int hullIntegrityAdded)
+		{
+			if ((currentHullIntegrity += hullIntegrityAdded) > maximumHullIntegrity) {
+				currentHullIntegrity = maximumHullIntegrity;
+			} 
+
+			else {
+				currentHullIntegrity += hullIntegrityAdded;
+			}
+		}
+
+		// This method is called if the ship's hull integrity is upgraded.
+		void upgradeHullIntegrity (int hullIntegrityUpgraded) 
+		{
+			maximumHullIntegrity += hullIntegrityUpgraded;
+		}
+
+	}
+}
+
---
+++ Assets/Resources/Oxygen.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+// The ship's oxygen supply is one of the resources that players of Cassiopeia need to keep track of.
+// The ship's oxygen supply is displayed as a percentage on a counter that will change color depending on how
+// the current oxygen level will affect crew members.
+
+namespace AssemblyCSharp 
+{
+	public class Oxygen
+	{
+		int percentage;
+
+		// This is the default constructor. By default, a ship starts with 100% oxygen.
+		public Oxygen()
+		{
+			percentage = 100;
+		}
+
+		// This constructor is for special situations when we want the ship to start with
+		// less than 100% oxygen.
+		public Oxygen(int startingPercentage)
+		{
+			percentage = startingPercentage;
+		}
+
+		// This is how we tick down oxygen via regular usage.
+		void useOxygen()
+		{
+			percentage -= 1;
+		}
+
+		// This is how we remove oxygen lost due to an event.
+		void loseOxygen(int percentLost) 
+		{
+			percentage -= percentLost;
+		}
+
+		// This is how we add oxygen via an event.
+		void addOxygen(int percentAdded)
+		{
+			percentage += percentAdded;
+		}
+	}
+}
