e86d9d855ed8c38b1351009fa6cc3b722c13d277
Jonah Rubin <jonah@jonaherubin.com>
2017-09-20 18:18:41 -0700

    added asteroid art, started on in-game events

---
+++ Assets/Entity Classes/Asteroid.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : Entity {
+	public float speed;
+	public float rotSpeed;
+	// Use this for initialization
+	void Start () {
+		rotSpeed *= (int)(Mathf.Round (Random.value * 2 - 1));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position -= new Vector3 (speed * Time.deltaTime, 0f, 0f);
+		transform.Rotate (0f, 0f, rotSpeed * Time.deltaTime);
+	}
+}
---
+++ Assets/Managers/EnviromentManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnviromentManager: MonoBehaviour {
+
+	public static EnviromentManager instance;
+
+	// Use this for initialization
+	void Awake () {
+		if (instance == null) {
+			instance = this;
+		} else {
+			Destroy (gameObject);
+		}
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
