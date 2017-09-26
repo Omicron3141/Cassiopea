873882aef80d2ec73ccd47bf279ccf0af2aaf394
Jonah Rubin <jonah@jonaherubin.com>
2017-09-26 12:26:50 -0700

    added system for creating events

--- Assets/Managers/EnviromentManager.cs
+++ Assets/Managers/EnviromentManager.cs
@@ -6,6 +6,11 @@ using UnityEngine.UI;
 public class EnviromentManager: MonoBehaviour {
 
 	public static EnviromentManager instance;
+	public GameObject[] events;
+	public float eventProbability;
+	public float meanDuration;
+	public float variationDuration;
+	private GameEvent currentEvent;
 
 	// Use this for initialization
 	void Awake () {
@@ -19,7 +24,19 @@ public class EnviromentManager: MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (currentEvent == null) {
+			if (Random.value < eventProbability) {
+				GameObject ev = Instantiate(events [Random.Range (0, events.Length)]);
+				currentEvent = ev.GetComponent<GameEvent> ();
+				ev.transform.position = new Vector3 (0f, 40f, 0f);
+				currentEvent.duration = meanDuration + (Random.value * 2 - 1) * variationDuration;
+			}
+		}
+	}
+
+
+	public void endEvent(){
+		currentEvent = null;
 	}
 
 }
